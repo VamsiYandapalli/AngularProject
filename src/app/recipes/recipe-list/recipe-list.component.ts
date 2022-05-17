55dea387cc17cfbca74593cc46be0be9ca917775
Vamsi Yandapalli <yandapalli_v@India.XoriantCorp.com>
2022-05-17 15:35:13 +0530

    Third Commit

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -14,7 +14,10 @@ export class RecipeListComponent implements OnInit {
     new Recipe('Test Recipe 2 ','This is a dummy description for the test recipe 2.',
     'https://i.guim.co.uk/img/media/91d098d57084a3988b5c6c6008837a32d85f8237/104_0_2557_1831/master/2557.jpg?width=465&quality=45&auto=format&fit=max&dpr=2&s=1c9a405468d1ede9c19ddcf16dbd4901'),
     new Recipe('Test Recipe 3','This is a dummy description for the test recipe 3.',
-    'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_1280.jpg')
+    'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_1280.jpg'),
+    new Recipe('Test Recipe 4 ','This is a dummy description for the test recipe 4.',
+    'https://i.guim.co.uk/img/media/91d098d57084a3988b5c6c6008837a32d85f8237/104_0_2557_1831/master/2557.jpg?width=465&quality=45&auto=format&fit=max&dpr=2&s=1c9a405468d1ede9c19ddcf16dbd4901')
+
   ];
   
   constructor() {
